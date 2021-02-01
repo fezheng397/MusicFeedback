ba2509f994e84a76ce6ca3f71083035ce7bff893
fezheng397 <fezheng397@gmail.com>
2021-01-31 19:59:12 -0500

    add wavesurfer

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useS3 } from 's3/context/S3Context';
+import RegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
 
 export default function Home({}) {
   const { getAudioFile, s3 } = useS3();
+  // i like me better 105.mp3
+  const audioFile = getAudioFile('Madeon - Pay No Mind Original_Mix.mp3');
   const [audioUrl, setAudioUrl] = useState('');
+  const [wavesurfer, setWavesurfer] = useState(undefined);
 
   useEffect(() => {
-    setAudioUrl(getAudioFile('i like me better 105.mp3'));
+    async function initAudio() {
+      const WaveSurfer = (await import('wavesurfer.js')).default;
+      const CursorPlugin = (
+        await import('wavesurfer.js/dist/plugin/wavesurfer.cursor.min.js')
+      ).default;
+
+      setAudioUrl(audioFile);
+      const xhr = {
+        cache: 'default',
+        mode: 'cors',
+        method: 'GET',
+        credentials: 'same-origin',
+      };
+      const wavesurfer = WaveSurfer.create({
+        container: document.querySelector('#waveform'),
+        waveColor: '#C2BFE6',
+        progressColor: '#4353FF',
+        cursorColor: '#4353FF',
+        barWidth: 3,
+        barRadius: 3,
+        cursorWidth: 1,
+        scrollParent: false,
+        height: 200,
+        barGap: 3,
+        plugins: [
+          CursorPlugin.create({
+            showTime: true,
+            opacity: 1,
+            customShowTimeStyle: {
+              'background-color': '#000',
+              color: '#fff',
+              padding: '2px',
+              'font-size': '10px',
+            },
+          }),
+          RegionsPlugin.create({
+            regionsMinLength: 2,
+            dragSelection: {
+              slop: 5,
+            },
+          }),
+        ],
+        xhr,
+      });
+      setWavesurfer(wavesurfer);
+      setTimeout(() => {
+        wavesurfer.load(
+          audioFile ||
+            'https://music-feedback-audio-files.s3.us-east-2.amazonaws.com/Madeon%20-%20Pay%20No%20Mind%20Original_Mix.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASZGDUSH72H3ARGGR%2F20210131%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20210131T174311Z&X-Amz-Expires=900&X-Amz-Signature=9b79402d42f449d8618bd81c19eff933de853148c8e65529f0aa3b33ce8c32db&X-Amz-SignedHeaders=host'
+        );
+      }, 500);
+    }
+
+    initAudio();
   }, [getAudioFile, s3]);
 
-  function playAudio() {
+  const playAudio = useCallback(() => {
     const audioEl = document.getElementsByClassName('audio-element')[0];
     console.log(audioEl);
     audioEl.play();
-  }
+    wavesurfer.play();
+  }, [wavesurfer]);
   console.log('Url:', audioUrl);
 
   return (
@@ -23,9 +81,15 @@ export default function Home({}) {
       <button onClick={playAudio}>
         <span>Play Audio</span>
       </button>
-      <audio className='audio-element' type='audio/mpeg'>
-        <source src={audioUrl}></source>
-      </audio>
+      <div
+        id='waveform'
+        style={{
+          width: '100%',
+          height: '100%',
+          marginTop: '200px',
+          position: 'relative',
+        }}
+      ></div>
     </div>
   );
 }
--- s3/setupS3Credentials.ts
+++ s3/setupS3Credentials.ts
@@ -4,5 +4,5 @@ export function setupS3Credentials() {
   AWS.config = new AWS.Config();
   AWS.config.accessKeyId = process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID;
   AWS.config.secretAccessKey = process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY;
-  AWS.config.region = 'us-west-1';
+  AWS.config.region = 'us-east-2';
 }
