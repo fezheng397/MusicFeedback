798a2ae4962460d9a5a30f3e72522a391afa31b9
fezheng397 <fezheng397@gmail.com>
2021-02-01 22:51:52 -0500

    add redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,32 @@ import 'styles/fonts/fonts.css';
 import { S3ContextProvider } from 's3/context/S3Context';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'constants/theme';
+import { Provider } from 'react-redux';
+import { initStore } from 'redux/store';
+import { StateLoader } from 'redux/store/stateLoader';
+
+declare global {
+  interface Window {
+    __SERVER_STATE__?: typeof Object;
+  }
+}
+
+const stateLoader = new StateLoader();
+
+export const store = initStore(
+  window.__SERVER_STATE__ || stateLoader.loadState()
+);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyleReset />
-      <S3ContextProvider>
-        <Component {...pageProps} />
-      </S3ContextProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyleReset />
+        <S3ContextProvider>
+          <Component {...pageProps} />
+        </S3ContextProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
